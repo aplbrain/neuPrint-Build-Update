0606cf32235a6b831c4118b2bfcf02daccc4f2b3
Lowell Umayam <umayaml@vm785.int.janelia.org>
2023-01-09 20:29:53 +0000

    added mapping of synapses to specified rois list

--- dvid_to_neuprint/export_dvid_synapses.py
+++ dvid_to_neuprint/export_dvid_synapses.py
@@ -10,13 +10,22 @@ tqdm = tqdm_notebook
 
 import numpy as np
 import pandas as pd
-from neuclease.dvid import find_master, fetch_synapses_in_batches, fetch_labels_batched
+from neuclease.dvid import find_master, fetch_synapses_in_batches, fetch_labels_batched, fetch_combined_roi_volume
+from neuclease.util import extract_labels_from_volume
 
 #example: python export_dvid_synapses.py emdata5-private.janelia.org:8510 52356 synapses segmentation
 server = sys.argv[1]
 uuid = sys.argv[2]
 annotation = sys.argv[3]
 segmentation_inst = sys.argv[4]
+all_rois_file = sys.argv[5]
+
+rois = []
+
+rois_list = open(all_rois_file,'r')
+for line in rois_list:
+    roi_name = line.rstrip('\n')
+    rois.append(roi_name)
 
 master = (server, uuid)
 
@@ -30,6 +39,7 @@ master = (server, uuid)
 # get as pandas dataframe
 #synapses_df, partners_df = fetch_synapses_in_batches(*master, annotation, box_zyx, format='pandas')
 synapses_df, partners_df = fetch_synapses_in_batches(*master, annotation, format='pandas')
+synapses_df.rename(columns={'kind': 'type'}, inplace=True)
 partners_df.rename(columns={'pre_id': 'synId_pre', 'post_id': 'synId_post'}, inplace=True)
 #partners_df.reset_index(inplace=True)
 print(partners_df)
@@ -52,8 +62,23 @@ master_seg = (server, uuid, segmentation_inst)
 
 labels = fetch_labels_batched(*master_seg, synapses_df[['z', 'y', 'x']].values, threads=32)
 synapses_df['body'] = labels
-
 synapses_df.reset_index(drop=True, inplace=True)
+
+# Map synapses to rois
+rois = { roi_name: label for label, roi_name in enumerate(rois, start=1) }
+print(rois)
+roi_vol, box, overlaps = fetch_combined_roi_volume(*master, rois, box_zyx=[(0,0,0), None])
+
+#from neuclease.dvid import load_synapses_csv
+#synapse_csv = sys.argv[3]
+#synapses_df = load_synapses_csv(synapse_csv)
+#print(f"Loaded {len(synapses_df)} points")
+extract_labels_from_volume(synapses_df, roi_vol, vol_scale=5, label_names=rois)
+
+synapses_df.rename(columns={'label_name': 'roi'}, inplace=True)
+
+synapses_df['roi'] = synapses_df['roi'].replace(['<unspecified>'], '')
+
 print(synapses_df)
 
 synapses_ftr = "synapses_" + uuid + ".ftr"
